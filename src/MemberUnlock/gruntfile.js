b7dc613de0fe8446ebf259a5919cb6a54651f1db
Michaël Vanbrabandt <info@mivaweb.be>
2018-03-06 19:08:32 +0100

    Add grunt and NPM

---
+++ src/MemberUnlock/gruntfile.js
@@ -0,0 +1,45 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+
+        // Clean the Site project /App_Plugins/MemberUnlock folder
+        clean: {
+            folder: ['../MemberUnlock.Site/App_Plugins/MemberUnlock'],
+            options: { force: true }
+        },
+
+        // Create /App_Plugins/MemberUnlock folder
+        mkdir: {
+            all: {
+                options: {
+                    create: ['../MemberUnlock.Site/App_Plugins/MemberUnlock']
+                }
+            }
+        },
+
+        // Copy files to the /App_Plugins/MemberUnlock folder
+        copy: {
+            files: {
+                cwd: 'Web/UI/App_plugins/MemberUnlock',
+                src: '**/*',
+                dest: '../MemberUnlock.Site/App_Plugins/MemberUnlock',
+                expand: true
+            }
+        },
+
+        // Watch when files are updated => copy to the Site project /App_Plugins/MemberUnlock folder
+        watch: {
+            memberlogin: {
+                files: ['Web/UI/App_plugins/MemberUnlock/**/*'],
+                tasks: ['copy']
+            }
+        }
+
+    });
+
+
+    // Load NPM tasks
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mkdir');
+};
