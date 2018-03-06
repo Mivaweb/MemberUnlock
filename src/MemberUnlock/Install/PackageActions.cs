5afb157978274427f0c42a6bd5db9ade358dea56
Michaël Vanbrabandt <info@mivaweb.be>
2018-03-06 20:35:32 +0100

    Update package action to add a scheduled task

--- src/MemberUnlock/Install/PackageActions.cs
+++ src/MemberUnlock/Install/PackageActions.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Configuration;
 using System.Xml;
 using umbraco.interfaces;
-using umbraco.IO;
+using Umbraco.Core.IO;
 
 namespace MemberUnlock.Install
 {
@@ -17,13 +18,22 @@ namespace MemberUnlock.Install
     {
         private const string APP_KEY = "memberLockedOutInMinutes";
 
+        private static string UmbracoConfig
+        {
+            get
+            {
+                return SystemDirectories.Config + "/umbracoSettings.config";
+            }
+        }
+
         public string Alias()
         {
-            return "MemberUnlock";
+            return "memberUnlock";
         }
 
         public bool Execute(string packageName, XmlNode xmlData)
         {
+            ExecuteUmbracoConfig();
             ExecuteWebConfig();
 
             return true;
@@ -31,12 +41,13 @@ namespace MemberUnlock.Install
 
         public XmlNode SampleXml()
         {
-            string sample = "<Action runat=\"install\" undo=\"true/false\" alias=\"MemberUnlock\"/>";
+            string sample = "<Action runat=\"install\" undo=\"true/false\" alias=\"" + Alias() + "\"/>";
             return umbraco.cms.businesslogic.packager.standardPackageActions.helper.parseStringToXmlNode(sample);
         }
 
         public bool Undo(string packageName, XmlNode xmlData)
         {
+            UndoUmbracoConfig();
             UndoWebConfig();
 
             return true;
@@ -64,6 +75,44 @@ namespace MemberUnlock.Install
         }
 
         /// <summary>
+        /// Add new scheduled task
+        /// </summary>
+        private void ExecuteUmbracoConfig()
+        {
+            bool saveFile = false;
+
+            // Get the umbracoSettings.config file
+            var configFile = IOHelper.MapPath(UmbracoConfig);
+
+            // Load in the xml
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(configFile);
+
+            var xnode = xdoc.SelectSingleNode("//scheduledTasks");
+
+            if(xnode != null)
+            {
+                var xmlTask = new StringBuilder();
+                xmlTask.AppendLine("<task log=\"true\" alias=\"memberUnlock\" interval=\"60\" url=\"/umbraco/backoffice/memberunlock/memberunlockapi/dounlock\" />");
+
+                // Create xml document of the StringBuilder
+                XmlDocument xmlNodeToAdd = new XmlDocument();
+                xmlNodeToAdd.LoadXml(xmlTask.ToString());
+
+                var nodeToAdd = xmlNodeToAdd.SelectSingleNode("*");
+
+                xnode.AppendChild(xnode.OwnerDocument.ImportNode(nodeToAdd, true));
+
+                saveFile = true;
+            }
+
+            if(saveFile)
+            {
+                xdoc.Save(configFile);
+            }
+        }
+
+        /// <summary>
         /// Remove key from appSettings section in the webConfig file
         /// </summary>
         private void UndoWebConfig()
@@ -80,6 +129,27 @@ namespace MemberUnlock.Install
                 }
             }
             catch { }
+        }
+
+        /// <summary>
+        /// Remove scheduled task
+        /// </summary>
+        private void UndoUmbracoConfig()
+        {
+            // Get the umbracoSettings.config file
+            var configFile = IOHelper.MapPath(UmbracoConfig);
+
+            // Load in the xml
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(configFile);
+
+            var xnode = xdoc.SelectSingleNode("//task [@alias='" + Alias() + "']");
+
+            if (xnode != null)
+            {
+                xdoc.SelectSingleNode("//scheduledTasks").RemoveChild(xnode);
+                xdoc.Save(configFile);
+            }
         }
 
         #endregion
